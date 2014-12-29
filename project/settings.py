cdf2a13a4872f5e975ef7c0f9148f76f584eab5b
Raphael Passini <raphapassini@gmail.com>
2014-12-28 23:06:27 -0200

    Start theme suport

--- project/settings.py
+++ project/settings.py
@@ -16,6 +16,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'themes'),
+)
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'kfxjcnogfq07z(774ira!llyo1t1i#zg)&+w*$8$87-7urd2ok'
 
